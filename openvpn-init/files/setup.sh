faf5bf04ced98afeb176fe0560bde7e922956cef
Nick Campion <nickcampion88@gmail.com>
2025-02-28 16:21:21 +0000

    updates

---
+++ openvpn-init/files/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Copying certs and configs"
+
+cp pki/private/server.key pki/ca.crt /etc/openvpn/server/
+cp pki/issued/server.crt /etc/openvpn/server/
+cp ta.key /etc/openvpn/server/
+cp pki/private/client1.key pki/issued/client1.crt ta.key /etc/openvpn/client/
+cp server.cnf /etc/openvpn/server/server.conf
+
+echo "Ready to start now..."
