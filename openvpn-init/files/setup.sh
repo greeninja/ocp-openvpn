706aea3c8ae5789785f1c88c560a06d7d61e74c7
Nick Campion <nickcampion88@gmail.com>
2025-02-28 16:26:50 +0000

    typo

--- openvpn-init/files/setup.sh
+++ openvpn-init/files/setup.sh
@@ -6,6 +6,6 @@ cp pki/private/server.key pki/ca.crt /etc/openvpn/server/
 cp pki/issued/server.crt /etc/openvpn/server/
 cp ta.key /etc/openvpn/server/
 cp pki/private/client1.key pki/issued/client1.crt ta.key /etc/openvpn/client/
-cp server.cnf /etc/openvpn/server/server.conf
+cp server.conf /etc/openvpn/server/server.conf
 
 echo "Ready to start now..."
