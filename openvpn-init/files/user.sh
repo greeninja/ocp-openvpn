76e60164860695dfe207dd03b592aa511de6ddd5
Nick Campion <nickcampion88@gmail.com>
2025-03-06 12:50:30 +0000

    clean up

--- openvpn-init/files/user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    sed "s,^openvpn.*,openvpn:x:$(id -u):0:openVPN:${HOME}:\/sbin\/nologin," /etc/passwd > /tmp/passwd
-    cat /tmp/passwd > /etc/passwd
-    rm /tmp/passwd
-  fi
-fi
-exec "$@"
